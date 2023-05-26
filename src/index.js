7c1e92950354d52b52de5e03e4b51a1f36924945
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-05-26 06:27:38 -0400

    rendering details for ramen

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
-const API = "http://localhost:3000/ramens";
-
+const API = "http://localhost:3000/ramens"
 
 fetch(API)
 .then((res) => res.json())
 .then(renderRamens);
 
-
 function renderRamens(ramens) {
-    ramens.forEach(renderRamen);
+    ramens.forEach(renderRamen)
 }
 
 function renderRamen(ramen) {
-    const ramenMenuDiv = document.getElementById("ramen-menu");
-    const ramenImage = document.createElement("img");
-    ramenImage.src = ramen.image;
-    ramenMenuDiv.append(ramenImage);
+    const ramenMenuDiv = document.getElementById("ramen-menu")
+    const ramenImage = document.createElement("img")
+    ramenImage.src = ramen.image
+    ramenMenuDiv.append(ramenImage)
+
+    ramenImage.addEventListener("click" , e => renderDetails(ramen))
+}
+
+function renderDetails(ramen) {
+    const detailImage = el("detail-image")
+    const ramenName = el("ramen-name")
+    const restaurantName = el("restaurant-name")
+    const ratingDisplay = el("rating-display")
+    const commentDisplay = el("comment-display")
+
+    detailImage.src = ramen.image
+    detailImage.alt = ramen.name
+    ramenName.textContent = ramen.name
+    restaurantName.textContent = ramen.restaurant
+    ratingDisplay.textContent = ramen.rating
+    commentDisplay.textContent = ramen.comment
+}
+
+function el(elementName) {
+    return document.getElementById(elementName)
 }
